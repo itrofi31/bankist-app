aa6f0118ddbe82472243554bc3fbe778d8677fc2
Ivan Trofimov <triv99@mail.ru>
2023-03-01 15:02:03 +0300

    init

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement:${i + 1} You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement:${i + 1} You witdrew ${Math.abs(mov)}`);
+//   }
+// });
